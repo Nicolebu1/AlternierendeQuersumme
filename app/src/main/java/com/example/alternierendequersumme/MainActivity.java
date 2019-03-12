df8d141bb836a7ffa98a6939af1032b3fe8c4f15
Nicole <nicolebu@edu.aau.at>
2019-03-12 09:27:46 +0100

    connected Views in xml with MainActivity

--- app/src/main/java/com/example/alternierendequersumme/MainActivity.java
+++ app/src/main/java/com/example/alternierendequersumme/MainActivity.java
@@ -9,10 +9,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView textview;
+    public TextView textview2;
+    public Button btn;
+    public EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textview = findViewById(R.id.textView);
+        textview2 = findViewById(R.id.textView2);
+        btn = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
     }
 }
